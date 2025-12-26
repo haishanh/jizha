532146488a6b1f230fefba5974e1eb8095d77c45
haishan <haishanhan@gmail.com>
2025-12-26 23:38:01 +0800

    Add jizha site url in the reply

--- src/lib/seqs/telegram.seq.ts
+++ src/lib/seqs/telegram.seq.ts
@@ -1,16 +1,14 @@
-import { TelegramService } from "@/lib/services/telegram.service";
-import * as config from "@/lib/config";
-import type { SeqHandlerInput } from "@/lib/utils/common.util";
-import assert from "assert";
+import * as config from '@/lib/config';
+import { TelegramService } from '@/lib/services/telegram.service';
+import type { SeqHandlerInput } from '@/lib/utils/common.util';
+import assert from 'assert';
 
 type SeqCtx = { jwt?: any; botId?: string; telegram?: TelegramService };
 
-export function setup(
-  input: SeqHandlerInput<{ telegram?: TelegramService; botId?: string }>,
-) {
-  const botId = input.ctx.botId || "0";
-  const botToken = config.get(["BOT_TOKEN", botId]);
-  assert(botToken, ["BOT_TOKEN", botId, "not configed"].join(" "));
+export function setup(input: SeqHandlerInput<{ telegram?: TelegramService; botId?: string }>) {
+  const botId = input.ctx.botId || '0';
+  const botToken = config.get(['BOT_TOKEN', botId]);
+  assert(botToken, ['BOT_TOKEN', botId, 'not configed'].join(' '));
   input.ctx.telegram = new TelegramService(botToken);
 }
 
@@ -18,7 +16,7 @@ export async function webhook(input: SeqHandlerInput<SeqCtx>) {
   const { jwt, telegram } = input.ctx;
   assert(jwt);
   assert(telegram);
-  const botId = input.ctx.botId || "0";
+  const botId = input.ctx.botId || '0';
 
   const body = await input.req.json();
   const { chat, text } = body.message;
@@ -28,7 +26,7 @@ export async function webhook(input: SeqHandlerInput<SeqCtx>) {
   const t = text?.trim();
 
   switch (t) {
-    case "/token":
+    case '/token':
       const token: string = await jwt.sign({ chatId: chat.id, botId });
       await telegram.sendMessage({
         chat_id: chat.id,
@@ -36,25 +34,22 @@ export async function webhook(input: SeqHandlerInput<SeqCtx>) {
       });
       await telegram.sendMessage({
         chat_id: chat.id,
-        text: "The above string is your notification token",
+        text: 'The above string is your notification token\\. Go checkout https://jizha\\.vercel\\.app',
       });
       break;
     default:
-      const candy =
-        botId === "jizha"
-          ? "\u53fd\u53fd\u55b3\u55b3\u624e\u624e\u4f60\u9e21\u9e21"
-          : "🍬🍬🍬";
+      const candy = botId === 'jizha' ? '\u53fd\u53fd\u55b3\u55b3\u624e\u624e\u4f60\u9e21\u9e21' : '🍬🍬🍬';
       await telegram.sendMessage({
         chat_id: chat.id,
         text: [
-          "😢",
+          '😢',
           "Hey, I don't know what to do with this command",
-          "Please send me something that I know",
+          'Please send me something that I know',
           candy,
-        ].join("\n\n"),
+        ].join('\n\n'),
         reply_markup: {
           resize_keyboard: true,
-          keyboard: [[{ text: "/token" }]],
+          keyboard: [[{ text: '/token' }]],
         },
       });
   }
